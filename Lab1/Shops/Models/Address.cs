beede3876cce78fb84bf46b27f2fa1ed3c518a79
Krugarrr <ttteeework@gmail.com>
2022-10-22 16:18:34 +0300

    initial commit: add address model

---
+++ Lab1/Shops/Models/Address.cs
@@ -0,0 +1,54 @@
+using Shops.Tools;
+
+namespace Shops.Models;
+
+public class Address : IEquatable<Address>
+{
+    private const int MinimalHouseNumber = 0;
+    public Address(string city, string street, int houseNumber)
+    {
+        ValidateAddress(city, street, houseNumber);
+        City = city;
+        Street = street;
+        HouseNumber = houseNumber;
+    }
+
+    public string City { get; }
+    public string Street { get; }
+    public int HouseNumber { get; }
+
+    public bool Equals(Address other)
+        => other is not null
+           && City.Equals(other.City)
+           && Street.Equals(other.Street)
+           && HouseNumber.Equals(other.HouseNumber);
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Address address)
+        {
+            return Equals(address);
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(City, Street, HouseNumber);
+    }
+
+    private void ValidateAddress(string city, string street, int houseNumber)
+    {
+        ArgumentNullException.ThrowIfNull(houseNumber);
+
+        if (string.IsNullOrEmpty(street) || string.IsNullOrWhiteSpace(street))
+            throw AddressException.WrongStreetAddressError();
+
+        if (string.IsNullOrEmpty(city) || string.IsNullOrWhiteSpace(city))
+            throw AddressException.WrongCityAddressError();
+
+        if (houseNumber < MinimalHouseNumber)
+            throw AddressException.WrongHouseAddressError(houseNumber);
+    }
+}
