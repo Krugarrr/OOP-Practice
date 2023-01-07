a1590f3bebd22395a891cd2f94a38970fa16f61d
Krugarrr <89861362+Krugarrr@users.noreply.github.com>
2023-01-07 21:08:07 +0300

    Add interface

---
+++ Lab5/Backups.Extra/LimitAlgorithms/ILimitAlgorithmStrategy.cs
@@ -0,0 +1,8 @@
+﻿using Backups.Entities;
+
+namespace Backups.Extra.LimitAlgorithms;
+
+public interface ILimitAlgorithmStrategy
+{
+    IReadOnlyList<RestorePoint> Execute(IEnumerable<ILimitAlgorithm> limitAlgorithms, IReadOnlyList<RestorePoint> restorePoints);
+}
